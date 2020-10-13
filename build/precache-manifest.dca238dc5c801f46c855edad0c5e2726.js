b61926f735ff9fa84182d09c14a825b58c79a28c
nicorubio96 <nicolasrubioolivares96@gmail.com>
2020-10-13 13:11:00 -0300

    okaa

---
+++ build/precache-manifest.dca238dc5c801f46c855edad0c5e2726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f25ed407f4a3bc00faf6b0a19989b8",
+    "url": "/nicorubio96/react-clima.git/index.html"
+  },
+  {
+    "revision": "556a759e3e2b67d64c67",
+    "url": "/nicorubio96/react-clima.git/static/css/main.b9d09034.chunk.css"
+  },
+  {
+    "revision": "aa64a99d879f33fca8fc",
+    "url": "/nicorubio96/react-clima.git/static/js/2.9143d7fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nicorubio96/react-clima.git/static/js/2.9143d7fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556a759e3e2b67d64c67",
+    "url": "/nicorubio96/react-clima.git/static/js/main.366bbf9d.chunk.js"
+  },
+  {
+    "revision": "6834f44d3b67404133df",
+    "url": "/nicorubio96/react-clima.git/static/js/runtime-main.82d25cbb.js"
+  }
+]);
